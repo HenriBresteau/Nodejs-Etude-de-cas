3df3fe629a8daf35b38fde97d8cacfda01adc6e7
Henri Bresteau <henri.bresteau@gmail.com>
2024-04-12 22:39:04 +0200

    [6] PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       },
+      error_file: "./logs/err.log", // Enregistre les erreurs dans le fichier err.log
+      instances: 3, // Lance 3 instances en parallèle
+      max_memory_restart: "200M", // Utilise un maximum de 200 Mo de mémoire
     },
   ],
 };
