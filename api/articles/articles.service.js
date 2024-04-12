5b0c4afc0651e874c808aa76a1fdde9c520c22e6
Henri Bresteau <henri.bresteau@gmail.com>
2024-04-12 18:28:24 +0200

    [3] Enpoint : Create / Update / Delete

---
+++ api/articles/articles.service.js
@@ -0,0 +1,19 @@
+const Article = require("./articles.schema");
+
+
+class ArticleService {
+
+  create(data) {
+    const article = new Article(data);
+    return article.save();
+  }
+  update(id, data) {
+    return Article.findByIdAndUpdate(id, data, { new: true });
+  }
+  delete(id) {
+    return Article.deleteOne({ _id: id });
+  }
+
+}
+
+module.exports = new ArticleService();
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const NotFoundError = require("./errors/not-found");
 const userRouter = require("./api/users/users.router");
 const usersController = require("./api/users/users.controller");
+const artilcesRouter = require("./api/articles/articles.router");
+const articleController = require("./api/articles/articles.controller");
 const authMiddleware = require("./middlewares/auth");
 require("./api/articles/articles.schema"); // temporaire
 const app = express();
@@ -13,16 +15,16 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  /*socket.on("my_event", (data) => {
+    console.log("a user connected");
+    /*socket.on("my_event", (data) => {
     console.log(data);
   });
   io.emit("event_from_server", { test: "foo" });*/
 });
 
 app.use((req, res, next) => {
-  req.io = io;
-  next();
+    req.io = io;
+    next();
 });
 
 app.use(cors());
@@ -30,24 +32,25 @@ app.use(express.json());
 
 app.use("/api/users", authMiddleware, userRouter);
 app.post("/login", usersController.login);
+app.use("/api/articles", artilcesRouter );
 
 app.use("/", express.static("public"));
 
 app.use((req, res, next) => {
-  next(new NotFoundError());
+    next(new NotFoundError());
 });
 
 app.use((error, req, res, next) => {
-  const status = error.status || 500;
-  const message = error.message;
-  res.status(status);
-  res.json({
-    status,
-    message,
-  });
+    const status = error.status || 500;
+    const message = error.message;
+    res.status(status);
+    res.json({
+        status,
+        message,
+    });
 });
 
 module.exports = {
-  app,
-  server,
+    app,
+    server,
 };
